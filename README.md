b493c0fa2f8ecc6891155b6433c65c3d8ec911d1
jbRotk <2436953050@qq.com>
2016-08-19 14:54:46 +0800

    2016/8/19

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # retrofit-rxjava-listview
+这里的测试链接为 : http://www.lukou.com/api/albums?album_tag=0&start=
--- app/src/main/java/com/example/administrator/rxjavaretrofitlistview/API/LuKouAPI.java
+++ app/src/main/java/com/example/administrator/rxjavaretrofitlistview/API/LuKouAPI.java
@@ -12,6 +12,10 @@ import rx.Observable;
 /**
  * Created by Administrator on 2016/8/19.
  */
+
+/*
+* 这里的URL地址为：http://www.lukou.com/api/albums?album_tag=0&start=
+ */
 public interface LuKouAPI {
     @GET("api/albums")
     Observable<LuKouBean> getFeed(@QueryMap Map<String,String> map);
